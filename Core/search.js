4760bb740b792b703bd9f220cfb08c9190044d67
acooper29 <157072340+acooper29@users.noreply.github.com>
2024-04-15 18:10:13 +0000

    web testing

--- Core/search.js
+++ Core/search.js
@@ -1,31 +1,31 @@
-d3.csv('SongCSV.csv').then(function (data) {
-    var songs = data;
-    var button = d3.select('#button');
-    var form = d3.select('#form');
+// d3.csv('SongCSV.csv').then(function (data) {
+//     var songs = data;
+//     var button = d3.select('#button');
+//     var form = d3.select('#form');
 
-    button.on('click', runEnter);
-    form.on('submit', function(event) {
-        event.preventDefault();  
-        runEnter();
-    });
+//     button.on('click', runEnter);
+//     form.on('submit', function(event) {
+//         event.preventDefault();  
+//         runEnter();
+//     });
 
-    function runEnter() {
-        d3.select('tbody').html(''); 
+//     function runEnter() {
+//         d3.select('tbody').html(''); 
 
-        var inputValue = d3.select('#user-input').property('value');
-        var filteredSongs = songs.filter(song => song.artists.includes(inputValue));
+//         var inputValue = d3.select('#user-input').property('value');
+//         var filteredSongs = songs.filter(song => song.artists.includes(inputValue));
         
-        var output = _.sortBy(filteredSongs, 'avg_vote').reverse();
+//         var output = _.sortBy(filteredSongs, 'avg_vote').reverse();
 
-        for (var i = 0; i < filteredSongs.length; i++) {
-            d3.select('tbody').append('tr').html(
-                '<td>' + (i+1) + '</td>' +
-                '<td>' + (output[i].song_title) + '</td>' +
-                '<td>' + (output[i].artist) + '</td>' +
-                '<td>' + (output[i].year) + '</td>' +
-                '<td>' + (output[i].album) + '</td>' 
-            );
-        }
-    }
-});
+//         for (var i = 0; i < filteredSongs.length; i++) {
+//             d3.select('tbody').append('tr').html(
+//                 '<td>' + (i+1) + '</td>' +
+//                 '<td>' + (output[i].song_title) + '</td>' +
+//                 '<td>' + (output[i].artist) + '</td>' +
+//                 '<td>' + (output[i].year) + '</td>' +
+//                 '<td>' + (output[i].album) + '</td>' 
+//             );
+//         }
+//     }
+// });
 
