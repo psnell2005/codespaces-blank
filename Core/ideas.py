61e1cedbb130ef54ffb54cd056b3e5dc1f76da9a
acooper29 <157072340+acooper29@users.noreply.github.com>
2024-04-16 21:05:00 +0000

    similar song fix

--- Core/ideas.py
+++ Core/ideas.py
@@ -17,19 +17,17 @@ model.fit(data[features])
 
 # Function to suggest similar songs based on a liked song
 def suggest_similar_songs(liked_song):
-    # Find the index of the liked song in the DataFrame
+    liked_song = liked_song.decode('utf-8') if isinstance(liked_song, bytes) else liked_song
+
     liked_song_index = data[data['Title'] == liked_song].index[0]
 
-    # Get the feature vector of the liked song
     liked_song_features = data.loc[liked_song_index, features].values.reshape(1, -1)
 
-    # Find the k nearest neighbors of the liked song
     distances, indices = model.kneighbors(liked_song_features)
 
     # Get the info of the similar songs
     similar_songs_info = [(data.loc[index, 'Title'], data.loc[index, 'ArtistName'], data.loc[index, 'Year'])for index in indices[0]]
 
-    # Remove the liked song itself from the suggestions
     similar_songs_info = similar_songs_info[1:]
 
     return similar_songs_info
