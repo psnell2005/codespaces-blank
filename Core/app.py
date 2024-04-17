85e8e007e190e0e371dd8e607750eb3824823061
acooper29 <157072340+acooper29@users.noreply.github.com>
2024-04-17 06:04:17 +0000

    static

--- Core/app.py
+++ Core/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template
 import os
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
