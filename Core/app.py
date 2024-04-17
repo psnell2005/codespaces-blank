4f3e3fdb645df1dd5ce7e3f9f03f8436be98e7c5
acooper29 <157072340+acooper29@users.noreply.github.com>
2024-04-17 05:23:48 +0000

    lotta changes

--- Core/app.py
+++ Core/app.py
@@ -1,16 +1,20 @@
 import csv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/data')
 def get_data():
     data = []
-    with open('SongCSV.csv', 'r') as file:
-        csv_reader = csv.reader(file)
+    with open('SongCSV.csv', 'r', newline='') as file:  
+        csv_reader = csv.DictReader(file)  
         for row in csv_reader:
             data.append(row)
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 8080)
+    app.run(debug=True, port=8080)
